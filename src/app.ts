e075d087a482bb7a45dfe90fa43cfb76f081e195
Komandr33 <and.kom2017@gmail.com>
2025-03-07 19:23:36 +0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { createServer } from 'node:http';
+
+const app = express();
+const server = createServer(app);
+
+app.get('/', (req: any, res: any) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(3009, () => {
+  console.log('server running at http://localhost:3009');
+});
